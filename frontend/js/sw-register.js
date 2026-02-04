af34e7a25783bcdd49fe33ea202d8d036d3b1f06
PineappleCarcass123 <pineapplecarcass123@gmail.com>
2026-02-03 21:12:40 -0500

    Delete frontend directory

--- frontend/js/sw-register.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js')
-            .then(reg => console.log('Service Worker Registered', reg.scope))
-            .catch(err => console.error('Service Worker Registration Failed', err));
-    });
-}
